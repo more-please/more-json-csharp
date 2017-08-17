9d1c29d773bbc5589d3727998e14661c6948b823
Iain Merrick <iain@iainmerrick.com>
2017-08-17 11:34:31 -0500

    Update JsonReader tests

--- Test/JsonReaderTest.cs
+++ Test/JsonReaderTest.cs
@@ -1,32 +1,33 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace More.Json.Test
 {
-    [TestFixture]
-    public class JsonReaderTest
-    {
-        [Test]
-        public void TestReadNumber()
-        {
-            Check(1, "1");
+	[TestFixture]
+	public class JsonReaderTest
+	{
+		[Test]
+		public void TestReadNumber()
+		{
+			Check(1, "1");
 			Check(-1, "-1");
 
-            Check(1.0, "1.0");
+			Check(1.0, "1.0");
 			Check(-1.0, "-1.0");
 			Check(1.0, "1e0");
 			Check(0.1, "1E-1");
 			Check(1200.0, "1.2e+3");
 
-            CheckFails("+1");
+			CheckFails("+1");
 			CheckFails("99999999999999999999999999999999999999999999999999999");
 		}
 
-        [Test]
-        public void TestReadString()
-        {
-            Check("", "\"\"");
+		[Test]
+		public void TestReadString()
+		{
+			Check("", "\"\"");
 			Check("Unicode escape: \0", "\"Unicode escape: \\u0000\"");
 			Check("\" \\ \b \f \n \r \t", "\"\\\" \\\\ \\b \\f \\n \\r \\t\"");
 			Check("Unicode: (╯°□°）╯︵ ┻━┻", "\"Unicode: (╯°□°）╯︵ ┻━┻\"");
@@ -42,7 +43,7 @@ namespace More.Json.Test
 		[Test]
 		public void TestReadArray()
 		{
-            IList<object> expected = new List<object> { 1, 2, 3, 4 };
+			IList<object> expected = new List<object> { 1, 2, 3, 4 };
 			Check(expected, "[1, 2, 3, 4]");
 
 			CheckFails("[1, 2, 3, 4");
@@ -53,8 +54,8 @@ namespace More.Json.Test
 		[Test]
 		public void TestReadDict()
 		{
-            // For the purposes of Equals(), ordering shouldn't matter
-            IDictionary<string, object> expected = new Dictionary<string, object> {
+			// For the purposes of Equals(), ordering shouldn't matter
+			IDictionary<string, object> expected = new Dictionary<string, object> {
 				{ "one", 1 },
 				{ "two", 2 },
 				{ "three", 3 },
@@ -62,24 +63,70 @@ namespace More.Json.Test
 				{ "five", 5 },
 			};
 
-            Check(expected, "{\"one\":1,\"two\":2,\"three\":3,\"four\":4,\"five\":5}");
+			Check(expected, "{\"one\":1,\"two\":2,\"three\":3,\"four\":4,\"five\":5}");
 
-            CheckFails("{");
-            CheckFails("{ 1: 2 }");
-            CheckFails("{ \"one\": 1 \"two\": 2 }");
-            CheckFails("{ \"one\": 1, \"two\": 2, }");
+			CheckFails("{");
+			CheckFails("{ 1: 2 }");
+			CheckFails("{ \"one\": 1 \"two\": 2 }");
+			CheckFails("{ \"one\": 1, \"two\": 2, }");
+		}
+
+		[Test]
+		public void TestListBuilder()
+		{
+			string json = " [ 1 , 2 , 3 ] ";
+
+			var arr = JsonReader.Read(json);
+			Assert.That(arr is List<object>);
+
+			var arr2 = JsonReader.Read(json, array:JsonReader.List);
+			Assert.That(arr is List<object>);
+			Assert.AreEqual(arr, arr2);
+
+			arr2 = JsonReader.Read(json, array: JsonReader.Immutable);
+			Assert.That(arr2 is ImmutableArray<object>);
+			Assert.AreEqual(arr, arr2);
+
+		}
+
+		[Test]
+		public void TestDictBuilder()
+		{
+			string json = " { \"foo\" : 1, \"bar\" : 2 } ";
+			var dict = JsonReader.Read(json);
+			Assert.That(dict is Dictionary<string, object>);
+
+			var dict2 = JsonReader.Read(json, dict: JsonReader.Dictionary);
+			Assert.That(dict2 is Dictionary<string, object>);
+			Assert.AreEqual(dict, dict2);
+
+			dict2 = JsonReader.Read(json, dict: JsonReader.Sorted);
+			Assert.That(dict2 is SortedDictionary<string, object>);
+			Assert.AreEqual(dict, dict2);
+
+			dict2 = JsonReader.Read(json, dict: JsonReader.Immutable);
+			Assert.That(dict2 is ImmutableDictionary<string, object>);
+			Assert.AreEqual(dict, dict2);
+
+			dict2 = JsonReader.Read(json, dict: JsonReader.ImmutableSorted);
+			Assert.That(dict2 is ImmutableSortedDictionary<string, object>);
+			Assert.AreEqual(dict, dict2);
+
+			dict2 = JsonReader.Read(json, dict: JsonReader.OrderPreserving);
+			Assert.That(dict2 is OrderPreservingDictionary<string, object>);
+			Assert.AreEqual(dict, dict2);
 		}
 
 		private void Check<T>(T expected, string json)
 		{
-            object obj = JsonReader.Read(json);
-            T result = (T)obj; // Should be able to cast result to the exact type
-            Assert.AreEqual(expected, result);
+			object obj = JsonReader.Read(json);
+			T result = (T)obj; // Should be able to cast result to the exact type
+			Assert.AreEqual(expected, result);
 		}
 
-        private void CheckFails(string json)
-        {
-            Assert.Throws<FormatException>(() => JsonReader.Read(json));
-        }
+		private void CheckFails(string json)
+		{
+			Assert.Throws<FormatException>(() => JsonReader.Read(json));
+		}
 	}
 }
