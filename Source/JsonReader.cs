5dc4126efd3796303e810bfce8054699947d7335
Iain Merrick <iain@iainmerrick.com>
2017-08-16 21:12:07 -0500

    Fix formatting

--- Source/JsonReader.cs
+++ Source/JsonReader.cs
@@ -6,340 +6,340 @@ using System.Globalization;
 
 namespace More.Json
 {
-    public class JsonReader : IDisposable
-    {
-        // ---------------------------------------------------------------------
-        // Constructor and convenience functions
+	public class JsonReader : IDisposable
+	{
+		// ---------------------------------------------------------------------
+		// Constructor and convenience functions
 
-        public JsonReader(TextReader reader, bool leaveOpen = false)
-        {
-            _reader = reader;
-            _dispose = !leaveOpen;
-        }
+		public JsonReader(TextReader reader, bool leaveOpen = false)
+		{
+			_reader = reader;
+			_dispose = !leaveOpen;
+		}
 
-        public static object Read(string str)
-        {
-            using (var r = new JsonReader(new StringReader(str)))
-                return r.ReadValue();
-        }
+		public static object Read(string str)
+		{
+			using (var r = new JsonReader(new StringReader(str)))
+				return r.ReadValue();
+		}
 
-        public static object Read(TextReader reader, bool leaveOpen = false)
-        {
-            using (var r = new JsonReader(reader, leaveOpen))
-                return r.ReadValue();
-        }
+		public static object Read(TextReader reader, bool leaveOpen = false)
+		{
+			using (var r = new JsonReader(reader, leaveOpen))
+				return r.ReadValue();
+		}
 
-        public static object Read(Stream stream, bool leaveOpen = false, int bufferSize = 4096)
-        {
-            var utf8 = new UTF8Encoding(
-                encoderShouldEmitUTF8Identifier: false,
-                throwOnInvalidBytes: true);
-            var reader = new StreamReader(stream, utf8, true, bufferSize, leaveOpen);
-            using (var json = new JsonReader(reader))
-                return json.ReadValue();
-        }
+		public static object Read(Stream stream, bool leaveOpen = false, int bufferSize = 4096)
+		{
+			var utf8 = new UTF8Encoding(
+				encoderShouldEmitUTF8Identifier: false,
+				throwOnInvalidBytes: true);
+			var reader = new StreamReader(stream, utf8, true, bufferSize, leaveOpen);
+			using (var json = new JsonReader(reader))
+				return json.ReadValue();
+		}
 
-        // ---------------------------------------------------------------------
-        // Override hooks
+		// ---------------------------------------------------------------------
+		// Override hooks
 
-        public virtual IDictionary<string, object> NewDict()
-        {
-            return new Dictionary<string, object>();
-        }
+		public virtual IDictionary<string, object> NewDict()
+		{
+			return new Dictionary<string, object>();
+		}
 
-        public virtual IList<object> NewArray()
-        {
-            return new List<object>();
-        }
+		public virtual IList<object> NewArray()
+		{
+			return new List<object>();
+		}
 
-        // ---------------------------------------------------------------------
-        // Read any JSON value
+		// ---------------------------------------------------------------------
+		// Read any JSON value
 
-        public object ReadValue()
-        {
-            Trim();
-            int c = Peek;
-            if (c < 0)
-            {
-                throw Error("unexpected end of input");
-            }
-            else if (c == '"')
-            {
-                return ReadString();
-            }
-            else if (c == '[')
-            {
-                return ReadArray();
-            }
-            else if (c == '{')
-            {
-                return ReadDict();
-            }
-            else if (c == '-' || (c >= '0' && c <= '9'))
-            {
-                return ReadNumber();
-            }
-            else if (c == 't')
-            {
-                Expect("true");
-                return true;
-            }
-            else if (c == 'f')
-            {
-                Expect("false");
-                return false;
-            }
-            else if (c == 'n')
-            {
-                Expect("null");
-                return null;
-            }
-            else
-            {
-                throw Error($"unexpected char: '{(char)c}'");
-            }
-        }
+		public object ReadValue()
+		{
+			Trim();
+			int c = Peek;
+			if (c < 0)
+			{
+				throw Error("unexpected end of input");
+			}
+			else if (c == '"')
+			{
+				return ReadString();
+			}
+			else if (c == '[')
+			{
+				return ReadArray();
+			}
+			else if (c == '{')
+			{
+				return ReadDict();
+			}
+			else if (c == '-' || (c >= '0' && c <= '9'))
+			{
+				return ReadNumber();
+			}
+			else if (c == 't')
+			{
+				Expect("true");
+				return true;
+			}
+			else if (c == 'f')
+			{
+				Expect("false");
+				return false;
+			}
+			else if (c == 'n')
+			{
+				Expect("null");
+				return null;
+			}
+			else
+			{
+				throw Error($"unexpected char: '{(char)c}'");
+			}
+		}
 
-        // ---------------------------------------------------------------------
-        // Read a specific JSON type
+		// ---------------------------------------------------------------------
+		// Read a specific JSON type
 
-        public object ReadNumber()
-        {
-            StartCapture(24);
+		public object ReadNumber()
+		{
+			StartCapture(24);
 
-            Maybe('-');
-            if (!Maybe('0'))
-                if (Maybe('1', '9'))
-                    while (Maybe('0', '9'))
-                        ;
+			Maybe('-');
+			if (!Maybe('0'))
+				if (Maybe('1', '9'))
+					while (Maybe('0', '9'))
+						;
 
-            bool isInt = true;
-            if (Maybe('.'))
-            {
-                isInt = false;
-                while (Maybe('0', '9'))
-                    ;
-            }
-            if (Maybe('e') || Maybe('E'))
-            {
-                isInt = false;
-                if (Maybe('+') || Maybe('-') || true)
-                    while (Maybe('0', '9'))
-                        ;
-            }
+			bool isInt = true;
+			if (Maybe('.'))
+			{
+				isInt = false;
+				while (Maybe('0', '9'))
+					;
+			}
+			if (Maybe('e') || Maybe('E'))
+			{
+				isInt = false;
+				if (Maybe('+') || Maybe('-') || true)
+					while (Maybe('0', '9'))
+						;
+			}
 
-            string s = EndCapture();
-            var c = CultureInfo.InvariantCulture;
+			string s = EndCapture();
+			var c = CultureInfo.InvariantCulture;
 
-            try
-            {
-                // Note: can't use ?: here, as the whole point is they're different types!
-                if (isInt)
-                    return Int32.Parse(s, c);
-                else
-                    return Double.Parse(s, c);
-            }
-            catch (Exception e)
-            {
-                throw Error(e);
-            }
-        }
+			try
+			{
+				// Note: can't use ?: here, as the whole point is they're different types!
+				if (isInt)
+					return Int32.Parse(s, c);
+				else
+					return Double.Parse(s, c);
+			}
+			catch (Exception e)
+			{
+				throw Error(e);
+			}
+		}
 
-        public string ReadString()
-        {
-            StringBuilder result = new StringBuilder();
-            Expect('"');
-            for (int c = Pop(); c >= 0; c = Pop())
-            {
-                if (c == '"')
-                {
-                    return result.ToString();
-                }
-                if (c == '\\')
-                {
-                    c = Pop();
-                    switch (c)
-                    {
-                        case '"': case '\\': case '/': break;
-                        case 'b': c = '\b'; break;
-                        case 'f': c = '\f'; break;
-                        case 'n': c = '\n'; break;
-                        case 'r': c = '\r'; break;
-                        case 't': c = '\t'; break;
-                        case 'u': c = Unicode(); break;
-                        default: throw Error("unknown escape code");
-                    }
-                }
-                result.Append((char)c);
-            }
-            throw Error("unterminated string");
-        }
+		public string ReadString()
+		{
+			StringBuilder result = new StringBuilder();
+			Expect('"');
+			for (int c = Pop(); c >= 0; c = Pop())
+			{
+				if (c == '"')
+				{
+					return result.ToString();
+				}
+				if (c == '\\')
+				{
+					c = Pop();
+					switch (c)
+					{
+						case '"': case '\\': case '/': break;
+						case 'b': c = '\b'; break;
+						case 'f': c = '\f'; break;
+						case 'n': c = '\n'; break;
+						case 'r': c = '\r'; break;
+						case 't': c = '\t'; break;
+						case 'u': c = Unicode(); break;
+						default: throw Error("unknown escape code");
+					}
+				}
+				result.Append((char)c);
+			}
+			throw Error("unterminated string");
+		}
 
-        public IList<object> ReadArray()
-        {
+		public IList<object> ReadArray()
+		{
 			var result = new JsonList();
-            Expect('[');
-            Trim();
-            if (Maybe(']'))
-            {
-                return result;
-            }
-            do
-            {
-                object item = ReadValue();
-                result.Add(item);
-                Trim();
-            }
-            while (Maybe(','));
-            Expect(']');
-            return result;
-        }
+			Expect('[');
+			Trim();
+			if (Maybe(']'))
+			{
+				return result;
+			}
+			do
+			{
+				object item = ReadValue();
+				result.Add(item);
+				Trim();
+			}
+			while (Maybe(','));
+			Expect(']');
+			return result;
+		}
 
-        public IDictionary<string, object> ReadDict()
-        {
+		public IDictionary<string, object> ReadDict()
+		{
 			var result = new JsonDict();
-            Expect('{');
-            Trim();
-            if (Maybe('}'))
-            {
-                return result;
-            }
-            do
-            {
-                Trim();
-                string key = ReadString();
-                Trim();
-                Expect(':');
-                object val = ReadValue();
-                result.Add(key, val);
-                Trim();
-            }
-            while (Maybe(','));
-            Expect('}');
-            return result;
-        }
+			Expect('{');
+			Trim();
+			if (Maybe('}'))
+			{
+				return result;
+			}
+			do
+			{
+				Trim();
+				string key = ReadString();
+				Trim();
+				Expect(':');
+				object val = ReadValue();
+				result.Add(key, val);
+				Trim();
+			}
+			while (Maybe(','));
+			Expect('}');
+			return result;
+		}
 
-        // ---------------------------------------------------------------------
-        // Intermediate parsing constructs
+		// ---------------------------------------------------------------------
+		// Intermediate parsing constructs
 
-        private char Unicode()
-        {
-            int result = 0;
-            for (int i = 0; i < 4; ++i)
-            {
-                int c = Pop();
-                if (c < 0)
-                {
-                    throw Error("Unterminated Unicode escape");
-                }
-                else if (c >= '0' && c <= '9')
-                {
-                    result = (16 * result) + (c - '0');
-                }
-                else if (c >= 'a' && c <= 'f')
-                {
-                    result = (16 * result) + (c - 'a' + 10);
-                }
-                else if (c >= 'A' && c <= 'F')
-                {
-                    result = (16 * result) + (c - 'A' + 10);
-                }
-                else
-                {
-                    throw Error($"Expected hex digit");
-                }
-            }
-            return (char)result;
-        }
+		private char Unicode()
+		{
+			int result = 0;
+			for (int i = 0; i < 4; ++i)
+			{
+				int c = Pop();
+				if (c < 0)
+				{
+					throw Error("Unterminated Unicode escape");
+				}
+				else if (c >= '0' && c <= '9')
+				{
+					result = (16 * result) + (c - '0');
+				}
+				else if (c >= 'a' && c <= 'f')
+				{
+					result = (16 * result) + (c - 'a' + 10);
+				}
+				else if (c >= 'A' && c <= 'F')
+				{
+					result = (16 * result) + (c - 'A' + 10);
+				}
+				else
+				{
+					throw Error($"Expected hex digit");
+				}
+			}
+			return (char)result;
+		}
 
-        private void Expect(string expected)
-        {
-            foreach (char c in expected)
-                Expect(c);
-        }
+		private void Expect(string expected)
+		{
+			foreach (char c in expected)
+				Expect(c);
+		}
 
-        private void Expect(char expected)
-        {
-            if (!Maybe(expected))
-                throw Error($"Expected '{expected}'");
-        }
+		private void Expect(char expected)
+		{
+			if (!Maybe(expected))
+				throw Error($"Expected '{expected}'");
+		}
 
-        private bool Maybe(char maybe)
-        {
-            if (Peek != maybe) return false;
-            Pop();
-            return true;
-        }
+		private bool Maybe(char maybe)
+		{
+			if (Peek != maybe) return false;
+			Pop();
+			return true;
+		}
 
-        private bool Maybe(char lo, char hi)
-        {
-            if (Peek < lo || Peek > hi) return false;
-            Pop();
-            return true;
-        }
+		private bool Maybe(char lo, char hi)
+		{
+			if (Peek < lo || Peek > hi) return false;
+			Pop();
+			return true;
+		}
 
-        private void Trim()
-        {
-            int c = Peek;
-            while (c >= 0 && char.IsWhiteSpace((char)c))
-            {
-                Pop();
-                c = Peek;
-            }
-        }
+		private void Trim()
+		{
+			int c = Peek;
+			while (c >= 0 && char.IsWhiteSpace((char)c))
+			{
+				Pop();
+				c = Peek;
+			}
+		}
 
-        public Exception Error(string message)
-        {
-            return new FormatException($"JsonReader error at index {_pos}: {message}");
-        }
+		public Exception Error(string message)
+		{
+			return new FormatException($"JsonReader error at index {_pos}: {message}");
+		}
 
-        public Exception Error(Exception cause)
-        {
-            return new FormatException($"JsonReader error at index {_pos}", cause);
-        }
+		public Exception Error(Exception cause)
+		{
+			return new FormatException($"JsonReader error at index {_pos}", cause);
+		}
 
-        // ---------------------------------------------------------------------
-        // Low-level parse functions
+		// ---------------------------------------------------------------------
+		// Low-level parse functions
 
-        private bool AtEnd => _reader.Peek() < 0;
-        private int Peek => _reader.Peek();
-        private int Pos => _pos;
+		private bool AtEnd => _reader.Peek() < 0;
+		private int Peek => _reader.Peek();
+		private int Pos => _pos;
 
-        private int Pop()
-        {
-            int result = _reader.Read();
-            if (result >= 0)
-            {
-                ++_pos;
-                _capture?.Append((char)result);
-            }
-            return result;
-        }
+		private int Pop()
+		{
+			int result = _reader.Read();
+			if (result >= 0)
+			{
+				++_pos;
+				_capture?.Append((char)result);
+			}
+			return result;
+		}
 
-        private void StartCapture(int expectedSize)
-        {
-            _capture = new StringBuilder(expectedSize);
-        }
+		private void StartCapture(int expectedSize)
+		{
+			_capture = new StringBuilder(expectedSize);
+		}
 
-        private string EndCapture()
-        {
-            string result = _capture.ToString();
-            _capture = null;
-            return result;
-        }
+		private string EndCapture()
+		{
+			string result = _capture.ToString();
+			_capture = null;
+			return result;
+		}
 
-        // ---------------------------------------------------------------------
-        // State
+		// ---------------------------------------------------------------------
+		// State
 
-        public void Dispose()
-        {
-            if (_dispose)
-                _reader.Dispose();
-        }
+		public void Dispose()
+		{
+			if (_dispose)
+				_reader.Dispose();
+		}
 
-        private readonly TextReader _reader;
-        private readonly bool _dispose;
-        private int _pos;
-        private StringBuilder _capture;
-    }
+		private readonly TextReader _reader;
+		private readonly bool _dispose;
+		private int _pos;
+		private StringBuilder _capture;
+	}
 }
--- Source/OrderPreservingDictionary.cs
+++ Source/OrderPreservingDictionary.cs
@@ -4,104 +4,104 @@ using System.Linq;
 
 namespace More.Json
 {
-    public class OrderPreservingDictionary<K, V> : IDictionary<K, V>
-    {
-        private readonly List<KeyValuePair<K, V>> _entries;
+	public class OrderPreservingDictionary<K, V> : IDictionary<K, V>
+	{
+		private readonly List<KeyValuePair<K, V>> _entries;
 
-        public OrderPreservingDictionary() : this(8) { }
+		public OrderPreservingDictionary() : this(8) { }
 
-        public OrderPreservingDictionary(int capacity)
-        {
-            _entries = new List<KeyValuePair<K, V>>(capacity);
-        }
+		public OrderPreservingDictionary(int capacity)
+		{
+			_entries = new List<KeyValuePair<K, V>>(capacity);
+		}
 
-        public bool IsReadOnly => false;
-        public int Count => _entries.Count;
+		public bool IsReadOnly => false;
+		public int Count => _entries.Count;
 
-        public ICollection<K> Keys => (from e in _entries select e.Key).ToArray<K>();
-        public ICollection<V> Values => (from e in _entries select e.Value).ToArray<V>();
+		public ICollection<K> Keys => (from e in _entries select e.Key).ToArray<K>();
+		public ICollection<V> Values => (from e in _entries select e.Value).ToArray<V>();
 
-        public void Add(K key, V val)
-        {
-            _entries.Add(new KeyValuePair<K, V>(key, val));
-        }
+		public void Add(K key, V val)
+		{
+			_entries.Add(new KeyValuePair<K, V>(key, val));
+		}
 
-        public void Add(KeyValuePair<K, V> entry)
-        {
-            _entries.Add(entry);
-        }
+		public void Add(KeyValuePair<K, V> entry)
+		{
+			_entries.Add(entry);
+		}
 
-        public bool Remove(KeyValuePair<K, V> e)
-        {
-            return _entries.Remove(e);
-        }
+		public bool Remove(KeyValuePair<K, V> e)
+		{
+			return _entries.Remove(e);
+		}
 
-        public bool Contains(KeyValuePair<K, V> e)
-        {
-            return _entries.Contains(e);
-        }
+		public bool Contains(KeyValuePair<K, V> e)
+		{
+			return _entries.Contains(e);
+		}
 
-        public void Clear()
-        {
-            _entries.Clear();
-        }
+		public void Clear()
+		{
+			_entries.Clear();
+		}
 
-        public void CopyTo(KeyValuePair<K, V>[] array, int i)
-        {
-            _entries.CopyTo(array, i);
-        }
+		public void CopyTo(KeyValuePair<K, V>[] array, int i)
+		{
+			_entries.CopyTo(array, i);
+		}
 
-        IEnumerator<KeyValuePair<K, V>> IEnumerable<KeyValuePair<K, V>>.GetEnumerator()
-        {
-            return _entries.GetEnumerator();
-        }
+		IEnumerator<KeyValuePair<K, V>> IEnumerable<KeyValuePair<K, V>>.GetEnumerator()
+		{
+			return _entries.GetEnumerator();
+		}
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return _entries.GetEnumerator();
-        }
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return _entries.GetEnumerator();
+		}
 
-        public V this[K key]
-        {
-            get
-            {
-                foreach (var e in _entries)
-                    if (key.Equals(e.Key))
-                        return e.Value;
-                throw new KeyNotFoundException($"Key not found: {key}");
-            }
-            set => Add(key, value);
-        }
+		public V this[K key]
+		{
+			get
+			{
+				foreach (var e in _entries)
+					if (key.Equals(e.Key))
+						return e.Value;
+				throw new KeyNotFoundException($"Key not found: {key}");
+			}
+			set => Add(key, value);
+		}
 
-        public bool TryGetValue(K key, out V val)
-        {
-            foreach (var e in _entries)
-                if (key.Equals(e.Key))
-                {
-                    val = e.Value;
-                    return true;
-                }
-            val = default(V);
-            return false;
-        }
+		public bool TryGetValue(K key, out V val)
+		{
+			foreach (var e in _entries)
+				if (key.Equals(e.Key))
+				{
+					val = e.Value;
+					return true;
+				}
+			val = default(V);
+			return false;
+		}
 
-        public bool ContainsKey(K key)
-        {
-            foreach (var e in _entries)
-                if (key.Equals(e.Key))
-                    return true;
-            return false;
-        }
+		public bool ContainsKey(K key)
+		{
+			foreach (var e in _entries)
+				if (key.Equals(e.Key))
+					return true;
+			return false;
+		}
 
-        public bool Remove(K key)
-        {
-            for (int i = 0; i < _entries.Count; ++i)
-                if (key.Equals(_entries[i].Key))
-                {
-                    _entries.RemoveAt(i);
-                    return true;
-                }
-            return false;
-        }
-    }
+		public bool Remove(K key)
+		{
+			for (int i = 0; i < _entries.Count; ++i)
+				if (key.Equals(_entries[i].Key))
+				{
+					_entries.RemoveAt(i);
+					return true;
+				}
+			return false;
+		}
+	}
 }
